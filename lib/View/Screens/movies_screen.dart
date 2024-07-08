5c8ec7734d9d75632ee0f127adb3165c436899f2
Karan Gupta <gupta.karan1162@gmail.com>
2024-07-08 12:06:14 +0530

    movies screen

--- lib/View/Screens/movies_screen.dart
+++ lib/View/Screens/movies_screen.dart
@@ -8,12 +8,31 @@ class MoviesScreen extends StatefulWidget {
 }
 
 class _MoviesScreenState extends State<MoviesScreen> {
+  List<dynamic> upcomingMovies = [];
+
+  bool isLoading = true;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Movies Screen"),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text("upcoming movies")),
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            )
+          : GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              children: List.generate(upcomingMovies.length, (index) {
+                final movie = upcomingMovies[index];
+                return GestureDetector(
+                  onTap: () {},
+                  child: Card(),
+                );
+              }),
+            ),
     );
   }
 }
