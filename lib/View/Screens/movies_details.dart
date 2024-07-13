de76ddff92db9a036e9ebe9a776640411ba5e983
Karan Gupta <gupta.karan1162@gmail.com>
2024-07-13 11:39:19 +0530

    movies details screen

---
+++ lib/View/Screens/movies_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MoviesDetailScreen extends StatefulWidget {
+  const MoviesDetailScreen({super.key});
+
+  @override
+  State<MoviesDetailScreen> createState() => _MoviesDetailScreenState();
+}
+
+class _MoviesDetailScreenState extends State<MoviesDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
