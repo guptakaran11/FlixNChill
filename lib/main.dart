0ef30c1ac626868bcbd9afb07c541916cd6befcb
Karan Gupta <gupta.karan1162@gmail.com>
2024-07-28 12:28:38 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'FLiXNChill',
       theme: ThemeData(
         brightness: Brightness.dark,
         primaryColor: Colors.white,
